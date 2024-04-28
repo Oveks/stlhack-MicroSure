268dfa0bfe6b21f0ed91c75194b5ca234468d7cd
Yuheng Ding <yuhengding0223@gmail.com>
2024-04-28 11:17:55 -0500

    frontend code to interaction with the smart contract

--- contract/src/js/app.js
+++ contract/src/js/app.js
@@ -2,20 +2,18 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
-  hasVoted: false,
 
   init: function() {
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    // TODO: refactor conditional
+    // Check if Web3 has been injected by the browser (MetaMask)
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
+      // If no injected web3 instance is detected, fallback to localhost
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -23,100 +21,78 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
-      // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election);
-      // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider);
+    $.getJSON('FlightDelayInsurance.json', function(insuranceArtifact) {
+      // Get the contract artifact file and instantiate it with truffle-contract
+      App.contracts.FlightDelayInsurance = TruffleContract(insuranceArtifact);
 
+      // Set the provider for our contract
+      App.contracts.FlightDelayInsurance.setProvider(App.web3Provider);
+
+      // Listen for events
       App.listenForEvents();
 
-      return App.render();
+      // Load account data
+      web3.eth.getCoinbase(function(err, account) {
+        if (err === null) {
+          App.account = account;
+          $("#accountAddress").html("Your Account: " + account);
+        }
+      });
+
+      // Render insurance history
+      App.renderInsuranceHistory();
     });
+
+    return App.bindEvents();
   },
 
-  // Listen for events emitted from the contract
+  bindEvents: function() {
+    $(document).on('submit', '#purchaseInsuranceForm', App.handlePurchaseInsurance);
+  },
+
   listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
-      // Restart Chrome if you are unable to receive this event
-      // This is a known issue with Metamask
-      // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance.votedEvent({}, {
-        fromBlock: 0,
-        toBlock: 'latest'
-      }).watch(function(error, event) {
-        console.log("event triggered", event)
-        // Reload when a new vote is recorded
-        App.render();
+    App.contracts.FlightDelayInsurance.deployed().then(function(instance) {
+      instance.InsurancePurchased({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event) {
+        console.log("Insurance purchased event triggered", event);
+        // Refresh insurance history
+        App.renderInsuranceHistory();
       });
     });
   },
 
-  render: function() {
-    var electionInstance;
-    var loader = $("#loader");
-    var content = $("#content");
+  handlePurchaseInsurance: function(event) {
+    event.preventDefault();
 
-    loader.show();
-    content.hide();
+    var airline = $("#airline").val();
+    var flightNumber = $("#flightNumber").val();
+    var date = $("#date").val();
+    var premium = $("#premium").val();
 
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
+    console.log("Purchasing insurance...", airline, flightNumber, date, premium);
 
-    console.log("here")
-
-    // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
-
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
-
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
-
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
-
-          // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          candidatesSelect.append(candidateOption);
-        });
-      }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
-        $('form').hide();
-      }
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
+    App.contracts.FlightDelayInsurance.deployed().then(function(instance) {
+      return instance.purchaseInsurance(airline, flightNumber, date, { value: web3.toWei(premium, 'ether'), from: App.account });
+    }).then(function(result) {
+      console.log("Insurance purchased", result);
+    }).catch(function(err) {
+      console.error(err);
     });
   },
 
-  castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
+  renderInsuranceHistory: function() {
+    App.contracts.FlightDelayInsurance.deployed().then(function(instance) {
+      return instance.getUserInsurances({ from: App.account });
+    }).then(function(insurances) {
+      var insuranceHistory = $("#insuranceHistory");
+      insuranceHistory.empty();
+
+      for (var i = 0; i < insurances.length; i++) {
+        var insurance = insurances[i];
+        var status = insurance[5] ? "Paid Out" : "Pending";
+
+        var insuranceTemplate = "<tr><td>" + (i + 1) + "</td><td>" + insurance[1] + "</td><td>" + insurance[2] + "</td><td>" + new Date(insurance[3] * 1000) + "</td><td>" + web3.fromWei(insurance[4], 'ether') + " ETH</td><td>" + status + "</td></tr>"
+        insuranceHistory.append(insuranceTemplate);
+      }
     }).catch(function(err) {
       console.error(err);
     });
